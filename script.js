38cd1d2ec8aa2acba7844065d5ebb1bc9e3eec54
Rohit-KumarRajak <rohitkrrajak993455@gmail.com>
2025-06-09 02:45:05 +0530

    Update render url script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function sendMessage() {
   addMessage("user", message);
   userInput.value = "";
 
-  fetch("http://127.0.0.1:5000/chat", {
+  fetch("https://zyakbot-backend.onrender.com/chat", {
 
     method: "POST",
     headers: { "Content-Type": "application/json" },
